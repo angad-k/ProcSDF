bb4ed1fc558a92a42d4f8059ed91b80386618cee
Angad Kambli <angadkambli@gmail.com>
2022-08-07 17:07:25 +0530

    start shader refactor

--- ProcSDF/Assets/Shaders/fragment.glsl
+++ ProcSDF/Assets/Shaders/fragment.glsl
@@ -1,20 +1,18 @@
+// PREAMBLE
+
 #version 330 core
 
-uniform vec3 cameraOrigin;
-uniform vec2 viewportSize;
-uniform float focalLength;
+uniform vec3 u_camera_origin;
+uniform vec2 u_viewport_size;
+uniform float u_focal_length;
 
 out vec4 FragColor;
 
-const bool DEBUG = false;
+const bool DEBUG = true;
 
-const int SPHERE_0 = 0;
-const int SPHERE_1 = 1;
-const int SPHERE_2 = 1;
+// END OF PREAMBLE
 
-int current_hit = -1;
-
-// ------------------------- RANDOMIZATION CODE (Picked up from stackoverflow : https://stackoverflow.com/a/17479300) 
+// RANDOMIZATION CODE (Picked up from stackoverflow : https://stackoverflow.com/a/17479300) 
 // A single iteration of Bob Jenkins' One-At-A-Time hashing algorithm.
 uint hash( uint x ) {
     x += ( x << 10u );
@@ -49,13 +47,15 @@ float random( vec2  v ) { return floatConstruct(hash(floatBitsToUint(v))); }
 float random( vec3  v ) { return floatConstruct(hash(floatBitsToUint(v))); }
 float random( vec4  v ) { return floatConstruct(hash(floatBitsToUint(v))); }
 
+// END OF RANDOMIZATION CODE
 
-// ------------------- END OF RANDOMIZATION CODE
+// UTILS
 
 vec2 random2(in vec2 uv)
 {
     float x = random(uv);
-    float y = random(vec2(uv.x + 1, uv.y));
+    float y = random(-normalize(uv)*x);
+    //float y = random(vec2(uv.x + 1, uv.y));
     return vec2(x, y);
 }
 
@@ -82,145 +82,201 @@ vec3 random_in_hemisphere(in vec3 normal, in vec3 intersection) {
         return -in_unit_sphere;
 }
 
-vec3 random_unit_vector(in vec2 uv)
-{
-    return normalize(random_in_unit_sphere(uv));
-}
+// END OF UTILS
 
-float distance_from_sphere(in vec3 p, in vec3 c, float r)
+// PRIMITIVES
+
+float sphere(in vec3 p, in vec3 c, float r)
 {
     return length(p - c) - r;
 }
+
+// END OF PRIMITIVES
+
+// STRUCTS
+
+struct closest_object_info
+{
+    float closest_distance;
+    int object_index;
+};
+
+// END OF STRUCTS
+
+// SCATTER FNS
 
 vec3 diffuse_scatter(in vec3 intersection, in vec3 normal)
 {
     return intersection + random_in_hemisphere(normal, intersection);
 }
 
+// END OF SCATTER FNS
 
-float smooth_union(in float p1, in float p2, in float transition)
+// OBJECT FNS
+
+float object_1(vec3 position)
 {
-    float h = clamp( 0.5 + 0.5*(p2-p1)/transition, 0.0, 1.0 );
-    return mix( p2, p1, h ) - transition*h*(1.0-h); 
+    return sphere(position, vec3(0.0, 0.0, -1.0), 0.5);
 }
 
-float map_the_world(in vec3 p)
+float object_2(vec3 position)
 {
-    float sphere_0 = distance_from_sphere(p, vec3(0.0, 0.0, -1.0), 0.5);
-    float sphere_2 = distance_from_sphere(p, vec3(0.25, 0.0, -1.0), 0.5);
-    float sphere_1 = distance_from_sphere(p, vec3(0,-100.5,-1), 100);
-    //float min_dist = smooth_union(sphere_2, sphere_0, 0.05);
-    float min_dist = sphere_0;
-    current_hit = SPHERE_0;
+    return 100000.0;
+}
+
+// END OF OBJECT FNS
+
+
+closest_object_info get_closest_object_info(vec3 position)
+{
+    float dist_1 = object_1(position);
+    float dist_2 = object_2(position);
     
-    return min_dist;
+    float min_dist = dist_1;
+
+    int object_index = 1;
+
+    /*if(dist_1 == min_dist)
+    {
+        object_index = 1;
+    }
+
+    if(dist_2 == min_dist)
+    {
+        object_index = 2;
+    }*/
+
+    return closest_object_info(min_dist, object_index);
 }
 
-vec3 calculate_normal(in vec3 p)
+vec3 calculate_normal(vec3 position, int object_index)
 {
     const vec3 small_step = vec3(0.001, 0.0, 0.0);
-    float gradient_x = map_the_world(p + small_step.xyy) - map_the_world(p - small_step.xyy);
-    float gradient_y = map_the_world(p + small_step.yxy) - map_the_world(p - small_step.yxy);
-    float gradient_z = map_the_world(p + small_step.yyx) - map_the_world(p - small_step.yyx);
-    vec3 normal = vec3(gradient_x, gradient_y, gradient_z);
-    return normalize(normal);
+    vec3 normal = vec3(1.0, 1.0, 1.0);
+    float g_x, g_y, g_z;
+    switch(object_index)
+    {
+        case 1:
+            g_x = object_1(position + small_step.xyy) - object_1(position - small_step.xyy);
+            g_y = object_1(position + small_step.yxy) - object_1(position - small_step.yxy);
+            g_z = object_1(position + small_step.yyx) - object_1(position - small_step.yyx);
+            normal = normalize(vec3(g_x, g_y, g_z));
+            break;
+        case 2:
+            g_x = object_2(position + small_step.xyy) - object_2(position - small_step.xyy);
+            g_y = object_2(position + small_step.yxy) - object_2(position - small_step.yxy);
+            g_z = object_2(position + small_step.yyx) - object_2(position - small_step.yyx);
+            normal = normalize(vec3(g_x, g_y, g_z));
+            break;
+    }
+    return normal;
 }
 
-vec3 ray_march(in vec3 ro, in vec3 rd, in int depth)
+vec3 get_target_ray(vec3 position, int object_index, vec3 normal)
+{
+    vec3 target = vec3(0.0, 0.0, 0.0);
+    switch(object_index)
+    {
+        case 1:
+            target = diffuse_scatter(position, normal);
+            break;
+        case 2:
+            target = diffuse_scatter(position, normal);
+            break;
+    }
+    return target;
+}
+
+vec3 get_color(vec3 position, int object_index)
+{
+    return vec3(0, 1.0, 0.0);
+}
+
+vec3 ray_march(in vec3 ray_origin, in vec3 ray_direction, in int depth)
 {
     float total_distance_traveled = 0.0;
     const int NUMBER_OF_STEPS = 16;
     const float MINIMUM_HIT_DISTANCE = 0.001;
     const float MAXIMUM_TRACE_DISTANCE = 1000.0;
-    vec3 color = vec3(1.0);
+    vec3 output_color = vec3(1.0);
     
-    while(depth >= 0)
-    {
-        bool breakLoop = false;
-
-        if(depth <= 0)
+   while(depth >= 0)
+   {
+        bool break_loop = false;
+        if(depth == 0)
         {
-            color *= 0.0;
-            
+            output_color *= 0.0;
             if(DEBUG)
             {
-                color = vec3(1.0, 0.0, 0.0);
+                output_color = vec3(1.0, 0.0, 0.0);
             }
-            break;
         }
 
         for (int i = 0; i < NUMBER_OF_STEPS; ++i)
         {
-            vec3 current_position = ro + total_distance_traveled * rd;
+            vec3 current_position = ray_origin + total_distance_traveled * ray_direction;
 
-            float distance_to_closest = map_the_world(current_position);
+            closest_object_info closest = get_closest_object_info(current_position);
 
-            if (distance_to_closest < MINIMUM_HIT_DISTANCE)
+            if (closest.closest_distance < MINIMUM_HIT_DISTANCE)
             {
-                vec3 normal = calculate_normal(current_position);
+                vec3 normal = calculate_normal(current_position, closest.object_index);
 
-                vec3 target = vec3(0.0);
-                
-                switch(current_hit)
-                {
-                    case SPHERE_0 :
-                        target = diffuse_scatter(current_position, normal);
-                        break;
-                    case SPHERE_2 :
-                        target = diffuse_scatter(current_position, normal);
-                        break;
-                }
+                vec3 target = get_target_ray(current_position, closest.object_index, normal);
 
-                color *= 0.5;
-                ro = current_position;
-                rd = target;
+                output_color *= get_color(current_position, closest.object_index);
+                ray_origin = current_position;
+                ray_direction = target;
                 break;
             }
 
             if (total_distance_traveled > MAXIMUM_TRACE_DISTANCE)
             {
-                vec3 unitDirection = normalize(rd);
-                float t = 0.5*(unitDirection.y + 1.0);
-                color *= (1.0-t)*vec3(1.0, 1.0, 1.0) + t*vec3(0.5, 0.7, 1.0);
-                breakLoop = true;
+                vec3 unit_direction = normalize(ray_direction);
+                float t = 0.5*(unit_direction.y + 1.0);
+                output_color *= (1.0-t)*vec3(1.0, 1.0, 1.0) + t*vec3(0.5, 0.7, 1.0);
+                if(DEBUG)
+                {
+                    return vec3(0.0, 1.0, 0.0);
+                }
+                break_loop = true;
                 break;
             }
-            total_distance_traveled += distance_to_closest;
+            total_distance_traveled += closest.closest_distance;
         }
-        
-        if(breakLoop)
+
+        if(break_loop)
         {
             break;
         }
         depth--;
-    }
+   }
 
-    return color;
+    return output_color;
 }
 
 void main()
 {
     int MAX_DEPTH = 50;
-    int SAMPLES = 50;
+    int SAMPLES = 10;
 
-    vec2 vSize = vec2(2.0*viewportSize.x/viewportSize.y, 2.0);
+    vec2 v_size = vec2(2.0*u_viewport_size.x/u_viewport_size.y, 2.0);
 
-    vec3 horizontal = vec3(vSize.x, 0.0, 0.0);
-    vec3 vertical = vec3(0.0, vSize.y, 0.0);
-    vec3 lowerLeftCorner = cameraOrigin - horizontal/2 - vertical/2 - vec3(0, 0, focalLength);
+    vec3 horizontal = vec3(v_size.x, 0.0, 0.0);
+    vec3 vertical = vec3(0.0, v_size.y, 0.0);
+    vec3 lower_left_corner = u_camera_origin - horizontal/2 - vertical/2 - vec3(0, 0, u_focal_length);
 
-    
-    vec3 shaded_color = vec3(0.0);
+    vec3 output_color = vec3(0.0);
 
-    vec2 randomVal = random2(gl_FragCoord.xy);
+    vec2 random_val = random2(gl_FragCoord.xy);
 
     for(int i = 0; i < SAMPLES; i++)
     {
-        vec2 uv = (gl_FragCoord.xy + randomVal)/viewportSize;
-        randomVal = random2(randomVal);
-        vec3 ro = cameraOrigin;
-        vec3 rd = lowerLeftCorner + uv.x*horizontal + uv.y*vertical - cameraOrigin; 
-        shaded_color += (ray_march(ro, rd, MAX_DEPTH)/SAMPLES);
+        vec2 uv = (gl_FragCoord.xy + random_val)/u_viewport_size;
+        random_val = random2(random_val);
+        vec3 ray_origin = u_camera_origin;
+        vec3 ray_direction = lower_left_corner + uv.x*horizontal + uv.y*vertical - u_camera_origin; 
+        output_color += (ray_march(ray_origin, ray_direction, MAX_DEPTH)/SAMPLES);
     }
-    FragColor = (vec4(shaded_color, 1.0));
+    FragColor = (vec4(output_color, 1.0));
 }
