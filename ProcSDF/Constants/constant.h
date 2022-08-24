f506c322ac426dfa2837ff52adad5b168feacc7f
ayushgupta138 <ayush.gupta02071@gmail.com>
2022-08-24 22:11:01 +0530

    Add Constant file

---
+++ ProcSDF/Constants/constant.h
@@ -0,0 +1,8 @@
+#pragma once
+#include <string>
+
+namespace sdf {
+	const std::string SHADER_FOLDER_PATH = "Assets/Shaders/";
+	const std::string SHADER_FILE_EXTENSION = ".glsl";
+	const std::string VERTEX_SHADER_PATH = "Assets/Shaders/vertex.glsl";
+}
--- ProcSDF/Rendering/ShaderGenerator.cpp
+++ ProcSDF/Rendering/ShaderGenerator.cpp
@@ -1,11 +1,10 @@
 #include "Rendering/ShaderGenerator.h"
+#include "Constants/constant.h"
 #include <fstream>
 #include <iostream>
 #include <string>
 #include <vector>
 
-const std::string FOLDER_PATH = "Assets/Shaders/";
-const std::string FILE_EXTENSION = ".glsl";
 // change the order of files according to the order of insertion in the generated fragment shader file.
 const std::vector<std::string> shader_files{"boilerplate", "primitive", "object_distance_functions", "ray_computation", "object_color", "ray_march", "main"};
 
@@ -17,18 +16,12 @@ void ShaderGenerator::generate_and_set_shader() {
 	std::string shader_string;
 	for (std::string shader_file : shader_files) {
 		std::string shader_component_string;
-		std::string file_path = FOLDER_PATH + shader_file + FILE_EXTENSION;
-		std::ifstream sourceFile(FOLDER_PATH + shader_file + FILE_EXTENSION);
+		std::string file_path = sdf::SHADER_FOLDER_PATH + shader_file + sdf::SHADER_FILE_EXTENSION;
+		std::ifstream sourceFile(sdf::SHADER_FOLDER_PATH + shader_file + sdf::SHADER_FILE_EXTENSION);
 		shader_component_string.assign((std::istreambuf_iterator< char >(sourceFile)), std::istreambuf_iterator< char >());
 		shader_string.append("\n\n" + shader_component_string);
 	}
-	//std::cout << "Finished creating\n"<<shader_string<<"\n";
-	ShaderGenerator::set_shader(shader_string);
-}
 
-void ShaderGenerator::write_shader_to_file(std::string file_path) {
-	std::ofstream out(file_path);
-	out << ShaderGenerator::get_shader();
-	out.close();
+	ShaderGenerator::set_shader(shader_string);
 }
 
