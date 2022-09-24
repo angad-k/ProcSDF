118ff34f138c6ac6b97515fa0245b27fdad230cd
Angad Kambli <angadkambli@gmail.com>
2022-09-25 02:16:32 +0530

    remove unecessary comment

--- ProcSDF/GUI/Nodes/Node.cpp
+++ ProcSDF/GUI/Nodes/Node.cpp
@@ -116,7 +116,6 @@ std::string Node::get_string()
 			std::string compilation_error = "Input is undefined for " + variable_name;
 			NodeGraph::get_singleton()->set_compilation_error(compilation_error);
 			ERR(compilation_error);
-			// set a global error variable here to show that graph cannot be computed.
 			return "ERR";
 		}
 		std::string input_arg_name = source_node->get_variable_name();
