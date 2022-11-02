698ac3ac6bb72f758a5ac6a0e1122b73d0557f3b
Angad Kambli <angadkambli@gmail.com>
2022-11-03 03:15:40 +0530

    make adding custom primitives possible

--- Assets/TestSDFs/octahedron.glsl
+++ Assets/TestSDFs/octahedron.glsl
@@ -1,5 +1,7 @@
 // type primitive
-float sdOctahedron( vec3 p, float s)
+// name Octahedron
+// float_params s
+float Octahedron( in vec3 p, in float s)
 {
   p = abs(p);
   return (p.x+p.y+p.z-s)*0.57735027;
